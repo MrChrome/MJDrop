f5fcbbdda71473a4005c026b33d4f80bf14756ed
Marc Santa <mrchrome@gmail.com>
2026-03-22 19:33:36 -0400

    Initial Commit

---
+++ MJDrop/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MJDrop
+//
+//  Created by MARC SANTA on 3/22/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MJDrop/MJDropApp.swift
@@ -0,0 +1,17 @@
+//
+//  MJDropApp.swift
+//  MJDrop
+//
+//  Created by MARC SANTA on 3/22/26.
+//
+
+import SwiftUI
+
+@main
+struct MJDropApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
